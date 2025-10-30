9e86cf2a96784a5dccb1ab1393735889fab49704
Yuki Sasa <s24c1059lh@s.chibakoudai.jp>
2025-10-30 13:28:01 +0900

    add test.bash

--- test.bash
+++ test.bash
@@ -1,13 +1,22 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Ryuichi Ueda 　　　　　
+# SPDX-License-Identifier: BSD-3-Clausei
 
 ng() {
 	echo ${1}行目が違うよ
 	res =1
+
 }
 
 res=0
-a=山田
-[ "$a" = 上田　] || ng "$LINENO"
-[ "$a" = 山田　] || ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+[ "${out}" = 15　] || ng "$LINENO"
+
+### STRANGE INPUT ###
+out=$(echo あ| ./plus)
+["$?" =1 ]    || ng "LINENO"
+[ "${out}" = ""] || ng "LINENO"
+
+[ "${res}" = 0　] && echo OK
 exit $res

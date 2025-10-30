277459c9924321e6796205ba7763c827361a3c1d
Yuki Sasa <s24c1059lh@s.chibakoudai.jp>
2025-10-30 14:11:41 +0900

    add test.bash

--- test.bash
+++ test.bash
@@ -9,8 +9,19 @@ ng() {
 }
 
 res=0
+
+### NOMAL INPUT ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng "LINENO"
+[ "${out}" = 15 ]   || ng "LINENO"
+
+### STRAGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]        || ng "LINENO"
+[ "${out}" = "" ]   || ng "LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]        || ng "LINENO"
+[ "${out}" = "" ]   || ng "LINENO" 
 
 [ "${res}" = 0 ] && echo OK
 exit $res
